83486718016ad591c2e5e4ec9f85fbc5e45e2594
quirogaemi87-collab <quirogaemi87@gmail.com>
2026-04-14 19:24:02 +0100

    Tarea de refactorizar y documentar realizada en las dos clases

---
+++ cuentas/CCuenta.java
@@ -0,0 +1,86 @@
+package cuentas;
+
+/**
+     * Clase que simula el funcionamiento de una cuenta bancaria
+     * Permite la gestion con numero de cuenta y nombre de la persona
+     * Muestra saldo disponible y tipo de interés.
+     */
+public class CCuenta {
+
+    /**
+     * nombre del titular de la cuenta
+     */
+    private String nombre;
+
+    /**
+     * numero de la cuenta bancaria
+     */
+    private String cuenta;
+
+    /**
+     * saldo de la cuenta
+     */
+    private double saldo;
+
+    /**
+     * interes
+     */
+    private double tipoInteres;
+
+    /**
+     * constructor vacio
+     */
+    public CCuenta(){
+    }
+
+    /**
+     * Constructor con parámetros
+     *
+     * @param nombre
+     * @param cuenta
+     * @param saldo
+     * @param tipoInteres
+     */
+    public CCuenta(String nombre, String cuenta, double saldo, double tipoInteres)
+    {
+        this.nombre =nombre;
+        this.cuenta =cuenta;
+        this.saldo =saldo;
+        this.tipoInteres = tipoInteres;
+    }
+
+        /**
+         * Devuelve el saldo
+         * @return
+         */
+    public double estado() {
+        return saldo;
+    }
+
+        /**
+         * Ingresa dinero en la cuenta
+         * @param cantidad es el importe para ingresar
+         * @throws Exception tiene que ser positivo
+         */
+    public void ingresar(double cantidad){
+        if (cantidad <= 0) {
+            throw new IllegalArgumentException("No se puede ingresar una cantidad negativa");
+        }
+            saldo += cantidad;
+    }
+
+        /**
+         * Retirar el dinero de la cuenta
+         * @param cantidad la cantidad que se va a retirar
+         * @throws Exception tiene que ser positivo y menor igual al saldo
+         */
+    public void retirar(double cantidad){
+            if (cantidad <= 0){
+                throw new IllegalArgumentException("No se puede retirar una cantidad negativa");
+            }
+            if (estado()< cantidad) {
+                throw new IllegalArgumentException("No se hay suficiente saldo");
+            }
+            saldo -= cantidad;
+    }
+}
---
+++ cuentas/Main.java
@@ -0,0 +1,58 @@
+package cuentas;
+
+/**
+ * clase que prueba la clase cuentas.CCuenta
+ */
+public class Main {
+
+    /**Entrada del programa
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        /**
+         * Se crea la cuenta bancaria
+         */
+        CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+
+        operativa_cuenta(cuenta1, 0.0f);
+    }
+
+    //llamar al metodo enviando informacion de cuenta1
+
+
+
+
+    /**
+     * metodo que engloba las operaciones de la cuenta
+     * @param cuenta1
+     * @param cantidad
+     */
+        public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+            /**
+             * muestra el saldo inicial
+             */
+            System.out.println("El saldo actual es"+ cuenta1.estado());
+
+            /**
+             * intento de retirar dinero
+             */
+            try {
+                cuenta1.retirar(2300);
+            } catch (Exception e) {
+                System.out.print("Fallo al retirar " + e.getMessage());
+            }
+
+            /**
+             * intento de ingresar dinero
+             */
+            try {
+                System.out.println("Ingreso en cuenta");
+                cuenta1.ingresar(695);
+            } catch (Exception e) {
+                System.out.print("Fallo al ingresar " + e.getMessage());
+            }
+        }
+    }
+
